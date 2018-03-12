b4ad4282aa475360f9af62f284073cc865b1ed2f
annettetran <annettetran@ucsb.edu>
2018-03-12 00:22:03 -0700

    Changed select to checkbox. Still need to fix outputs. Shoreline is still overlapping.

--- app.R
+++ app.R
@@ -10,7 +10,6 @@ library(devtools)
 library(maptools)
 
 dust <- st_read(dsn = ".", layer = "DustEmiss")
-# "." means look in the working directory
 
 dust_df <- st_transform(dust, "+init=epsg:4326")
 
@@ -22,6 +21,11 @@ emiss <- dust_df %>%
 shoreline <- st_read(dsn = ".", layer = "HiResBathy")
 shoreline_df <- st_transform(shoreline, "+init=epsg:4326")
 
+#shoreline_df2 <- subset(shoreline_df, !(Year == "2046")) %>% 
+  #arrange(Year)
+
+#shoreline_df2$Acres<- acres$CumulativeExposed
+
 playa <- shoreline_df %>%
   select(Year)
 
@@ -32,36 +36,39 @@ ui <- fluidPage(
   
   titlePanel("Salton Sea Future Shoreline & Emissivity"),
   
-  # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
       
-      sliderInput("shore", "Future Shoreline of the Sea", min = 2018, max = 2047, value = 2018, step = 5),
-      selectInput("dusty", "Emissivity of Exposed Playa", choices = unique(emiss$Emiss_Cat))
+      sliderInput("shore", "Select Year", min = 2018, max = 2047, value = 2018, step = 5),
+      #selectInput("dusty", "Emissivity of Exposed Playa", choices = unique(emiss$Emiss_Cat))
+      checkboxGroupInput("dusty", "Select Emissivity of Exposed Playa", choices = unique(emiss$Emiss_Cat), selected = "Least Emissive")
+      
+      #"sexe","Sexe:", 
+      #choices = c("Masculin" = "mas", "Femenin" = "fem"),
+      #selected = c("mas","fem")
       
       
     ),
     
-    # Show a plot of the generated distribution
     mainPanel(
-      leafletOutput("full_map"),
-      textOutput("shoreline")
+      leafletOutput("full_map")
+    #  box(textOutput("acreage"))
       
       )
   )
 )
 
-# Define server logic required to draw an interactive map
+
 server <- function(input, output) {
   
   output$full_map <- renderLeaflet({
-     emiss_sub <- emiss %>%
+    emiss_sub <- emiss %>%
     filter(Emiss_Cat == input$dusty)
     
   leaflet(emiss_sub) %>% 
       addProviderTiles("Stamen.Terrain") %>% 
     addPolygons(weight = 0.5,
-                color = "red",
+                color = input$dusty,
                 fillColor = "red",
                 fillOpacity = 1)  
    
@@ -76,19 +83,19 @@ server <- function(input, output) {
        addPolylines(weight = 3, color = "white", opacity = 1)
   
   })
-  acre_sub <- acres %>% 
-    filter(Year == input$shore)
   
- output$shoreline <- renderText({acre_sub
+  #output$acreage <- renderText(
+  # acres2 <- playa %>% 
+  #  filter(Year == input$shore)
+  
+  #box(acres2)
+  
+  
+  #)
    
- })
-    
-    
+ }
     
 
-  
-  
-}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
